b793c748900b8646b2e27eed4ce6d122851e0fcc
D_SAI_KIRAN_REDDY <dasappagarisaikiranreddy@gmail.com>
2025-05-07 20:28:11 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("branch: mybranch");
         System.out.println("This is my first version");
+        System.out.println("Edited in github");
     } 
 }
